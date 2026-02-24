2e6066c3092e03efa31a9b879422f9a779c7d7b4
Nguyen Le Tin Tri <nlttri.brc@gmail.com>
2026-02-24 17:30:32 +0700

    24/2/2026

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,16 +2,13 @@ import 'dotenv/config';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from "@/app/generated/prisma/client";
 
-const adapter = new PrismaMariaDb(
-  {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    connectionLimit: 5,
-  },
-)
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const adapter = new PrismaMariaDb({
+  host: "localhost",
+  port: 3306,
+  connectionLimit: 151,
+});
 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient({ adapter });
